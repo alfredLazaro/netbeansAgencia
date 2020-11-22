ed6aa2d7e7a175f08ccf07ab061469db8e8bd881
fred <alfredoLazaro999@gmail.com>
2020-11-22 14:55:43 -0400

    si la anterior a esta no se guardo con el commit sobre net bes lo hago de nuevo

--- AgenciaDeViajes/src/modelo/XMLClientes.java
+++ AgenciaDeViajes/src/modelo/XMLClientes.java
@@ -4,11 +4,14 @@
  * and open the template in the editor.
  */
 package modelo;
-
-/**
- *
- * @author USUARIO
- */
+import java.util.ArrayList;
+import java.util.List;
+import metodosAgen.*;
 public class XMLClientes {
-    
+    private static final String nomArchivo="clientes";
+    public static void main(String[] args) {
+        List<Cliente> listaClientes=new ArrayList<>();
+        Cliente cliente1=new Cliente("rebeca ",3,"verano","",12345);
+       
+    }
 }
